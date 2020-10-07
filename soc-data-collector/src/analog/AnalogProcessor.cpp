00d533bcb2b7a63732983e280b08b8d34a6de1d3
mangoon91 <leon.brendel@web.de>
2020-10-07 17:51:22 -0400

    Added the readout of a second pin.

--- soc-data-collector/src/analog/AnalogProcessor.cpp
+++ soc-data-collector/src/analog/AnalogProcessor.cpp
@@ -1,16 +1,22 @@
 #include <Arduino.h>
 
-int sensorPin = A3;
-int sensorValue = 0;
+int sensorPin3 = A3;
+int sensorPin4 = A4;
+int sensorValue3 = 0;
+int sensorValue4 = 0;
 
 void InitializeAnalogSensor() {
 
 }
 
 void ReadAnalogData() {
-    sensorValue = analogRead(sensorPin);
-    float voltage= sensorValue * (5.0 / 1023.0);
+    sensorValue3 = analogRead(sensorPin3);
+    sensorValue4 = analogRead(sensorPin4);
+    float voltage3 = sensorValue3 * (5.0 / 1023.0);
+    float voltage4 = sensorValue4 * (5.0 / 1023.0);
     Serial.print("A ");
-    Serial.print(voltage);
+    Serial.print(voltage3);
+    Serial.print(" A ");
+    Serial.print(voltage4);
     Serial.println("");
 }
